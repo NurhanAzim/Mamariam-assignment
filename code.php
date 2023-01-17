59e8c9b2d80288832f513e3132c83347af54b0f4
Azim <azim.nurhan.ahmad@gmail.com>
2023-01-17 15:28:34 +0800

    no cmment

--- code.php
+++ code.php
@@ -5,31 +5,31 @@ require_once __DIR__ . '/includes/composer/vendor/autoload.php';
 if (isset($_POST['confirm_order-btn'])) {
     $randomBytes = random_bytes(4);
     $customerId = unpack('N', $randomBytes)[1] & 0x7FFFFFFF;
-    $customerId = $customerId % 1000000000;//generate 9 random number
-    $checkCustomerId = "SELECT `customerId` FROM `tbl_customer` WHERE `customerId`='$customerId'";
-    $checkCustomerId_run = $conn->query($checkCustomerId);
+    $customerId = $customerId % 1000000000; //generate 9 random number
 
-    if (mysqli_num_rows($checkCustomerId_run) == 0) {
-        $fname = mysqli_real_escape_string($conn, $_POST['fname']);
-        $lname = mysqli_real_escape_string($conn, $_POST['lname']);
-        $address = mysqli_real_escape_string($conn, $_POST['address']);
-        $state = mysqli_real_escape_string($conn, $_POST['state']);
-        $city = mysqli_real_escape_string($conn, $_POST['city']);
-        $postcode = mysqli_real_escape_string($conn, $_POST['postcode']);
-        $telNo = mysqli_real_escape_string($conn, $_POST['telNo']);
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+    $address = mysqli_real_escape_string($conn, $_POST['address']);
+    $state = mysqli_real_escape_string($conn, $_POST['state']);
+    $city = mysqli_real_escape_string($conn, $_POST['city']);
+    $postcode = mysqli_real_escape_string($conn, $_POST['postcode']);
+    $telNo = mysqli_real_escape_string($conn, $_POST['telNo']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
 
-        $query = "INSERT INTO `tbl_customer`(`customerId`, `first_name`, `last_name`, `address`, `city`, `state`, `postcode`, `telNo`, `email`) VALUES ('$customerId', '$fname', '$lname', '$address', '$city', '$state', '$postcode', $telNo, '$email') LIMIT 1";
+    $createCustomer = "INSERT INTO `tbl_customer`(`customerId`, `first_name`, `last_name`, `address`, `city`, `state`, `postcode`, `telNo`, `email`) VALUES ('$customerId', '$fname', '$lname', '$address', '$city', '$state', '$postcode', $telNo, '$email') LIMIT 1";
+    $createCustomer_run = $conn->query($createCustomer);
+    if ($createCustomer_run) {
+        $query = "INSERT INTO `tbl_order` (`productId`, `orderQuantity`, `totalPrice`, `customerId`) VALUES ('" . $_POST['productId'] . "', 1, '" . $_POST['productPrice'] . "', '$customerId')";
         $query_run = $conn->query($query);
-    }
-    $query = "INSERT INTO `tbl_order` (`productId`, `orderQuantity`, `totalPrice`, `customerId`) VALUES ('" . $_POST['productId'] . "', 1, '" . $_POST['productPrice'] . "', '$customerId')";
-    $query_run = $conn->query($query);
 
-    if ($query_run) {
-        header("Location: index.php");
-        exit(0);
+        if ($query_run) {
+            header("Location: index.php");
+            exit(0);
+        } else {
+            header("Location: checkout.php");
+            exit(0);
+        }
     } else {
-        header("Location: checkout.php");
-        exit(0);
+        echo $createCustomer;
     }
 }
